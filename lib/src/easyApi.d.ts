7b7096377a43d0b457952c9ea6ae3ea7d7c53f24
eveffer <eliveffer@gmail.com>
2024-08-03 17:49:27 +0300

    checkpoint

--- lib/src/easyApi.d.ts
+++ lib/src/easyApi.d.ts
@@ -1,5 +1,13 @@
 export declare class EasyApi {
-    constructor();
-    fetch(): Promise<any>;
+    host: string;
+    private notify;
+    constructor(host?: string);
+    call<T extends Record<string, any>>(group: string, action: string, data?: Record<string, any>): Promise<T>;
+    private parseError;
+    onNotify(callback: (info: {
+        message: string;
+        title: string;
+        type: string;
+    }) => void): void;
 }
 //# sourceMappingURL=easyApi.d.ts.map
